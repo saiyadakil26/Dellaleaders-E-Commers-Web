1ce36a26a716280affed0ea66fea658a3b538d47
Saiyad Mahammadakil <saiyadakil26@gmail.com>
2022-08-19 17:50:55 +0530

    Error Solve

--- pages/Navbar.js
+++ pages/Navbar.js
@@ -1,19 +1,20 @@
 import Image from "next/image"
 import img from '../public/favicon.svg'
+import {useRouter} from 'next/router'
 
 export default function Navbar() {
-    return (
-        
-      <div className="md:mx-[15%] flex sticky top-0 p-2 bg-black">
-       <a href="/"> <img src="https://www.dellaleaders.com/images/logo.svg" width="50px"></img></a>
-        <div className="flex space-x-6 items-center justify-end flex-grow">
+  const rout = useRouter();
+  return (
+
+    <div className="md:mx-[15%] flex sticky top-0 p-2 bg-black">
+      <img src="https://www.dellaleaders.com/images/logo.svg" onClick={() =>rout.push('/')} width="50px" className=" cursor-pointer"></img>
+      <div className="flex space-x-6 items-center justify-end flex-grow">
         <p className="cursor-pointer">Home</p>
         <p className="cursor-pointer">Product</p>
         <p className="cursor-pointer">About Me</p>
         <p>|</p>
-        <a href="/Login"><button className=" p-2 bg-red-600 rounded-xl w-16 border-solid border-2 border-black hover:border-white hover:bg-black ">Login</button></a>
-        </div>
+        <button className=" p-2 bg-red-600 rounded-xl w-16 border-solid border-2 border-black hover:border-white hover:bg-black " onClick={()=>rout.push('/Login')}>Login</button>
       </div>
-    )
-  }
-  
+    </div>
+  )
+}
